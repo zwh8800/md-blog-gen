8894ead8452c49fa7202fef0da8286630ad34f44
Zhang Weihu <496781108@qq.com>
2016-04-04 22:27:28 +0800

    log

--- index/note.go
+++ index/note.go
@@ -6,6 +6,8 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang/glog"
+
 	"github.com/zwh8800/md-blog-gen/conf"
 	"github.com/zwh8800/md-blog-gen/render"
 	"github.com/zwh8800/md-blog-gen/service"
@@ -15,11 +17,13 @@ func Note(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseInt(idStr, 10, 64)
 	if err != nil {
+		glog.Error(err)
 		errorHandler(c, http.StatusNotFound, errors.New("Not Found"))
 		return
 	}
 	note, err := service.NoteById(id)
 	if err != nil {
+		glog.Error(err)
 		errorHandler(c, http.StatusNotFound, errors.New("Not Found"))
 		return
 	}
