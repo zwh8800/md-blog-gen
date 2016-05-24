afb45e23c05ece2c20f13ad37580c5d4cb764459
Zhang Weihu <496781108@qq.com>
2016-05-24 13:12:16 +0800

    add boost

--- service/note_search.go
+++ service/note_search.go
@@ -6,45 +6,18 @@ import (
 
 	"gopkg.in/olivere/elastic.v3"
 
-	"github.com/zwh8800/md-blog-gen/dao"
 	"github.com/zwh8800/md-blog-gen/model"
 )
-
-func NoteByKeyword(keyword string, page, limit int64) ([]*model.Note, map[int64][]*model.Tag, int64, error) {
-	sess := dbConn.NewSession(nil)
-
-	noteCount, err := dao.CountNoteByKeyword(sess, keyword)
-	if err != nil {
-		return nil, nil, 0, err
-	}
-	maxPage := (noteCount-1)/limit + 1
-
-	page-- //数据库层的页数从0开始数
-	noteList, err := dao.NoteByKeyword(sess, keyword, page, limit)
-	if err != nil {
-		return nil, nil, 0, err
-	}
-	if len(noteList) == 0 {
-		return noteList, nil, maxPage, nil
-	}
-
-	noteIdList := make([]int64, 0, len(noteList))
-	for _, note := range noteList {
-		noteIdList = append(noteIdList, note.Id)
-	}
-	tagListMap, err := dao.TagsByNoteIds(sess, noteIdList)
-	if err != nil {
-		return nil, nil, 0, err
-	}
-	return noteList, tagListMap, maxPage, nil
-}
 
 func SearchNoteByKeyword(keyword string, page, limit int64) ([]*model.SearchedNote, int64, error) {
 	page-- //数据库层的页数从0开始数
 	offset := page * limit
 
-	query := elastic.NewMultiMatchQuery(keyword,
-		"notename", "title", "content", "tagList")
+	query := elastic.NewMultiMatchQuery(keyword).
+		FieldWithBoost("notename", 1).
+		FieldWithBoost("tagList", 2).
+		FieldWithBoost("content", 4).
+		FieldWithBoost("title", 4)
 	highlight := elastic.NewHighlight().
 		Field("content").
 		Field("title").
