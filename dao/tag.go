347b686be23071edda2cf6d761f3cfe850102ebf
Zhang Weihu <496781108@qq.com>
2016-07-23 11:41:31 +0800

    sr

--- dao/tag.go
+++ dao/tag.go
@@ -6,36 +6,36 @@ import (
 	"github.com/zwh8800/md-blog-gen/model"
 )
 
-func TagById(sess *dbr.Session, id int64) (*model.Tag, error) {
+func TagById(sr dbr.SessionRunner, id int64) (*model.Tag, error) {
 	tag := &model.Tag{}
-	if err := sess.Select("*").From(model.TagTableName).
+	if err := sr.Select("*").From(model.TagTableName).
 		Where("id = ?", id).LoadStruct(tag); err != nil {
 		return nil, err
 	}
 	return tag, nil
 }
 
-func TagByName(sess *dbr.Session, name string) (*model.Tag, error) {
+func TagByName(sr dbr.SessionRunner, name string) (*model.Tag, error) {
 	tag := &model.Tag{}
-	if err := sess.Select("*").From(model.TagTableName).
+	if err := sr.Select("*").From(model.TagTableName).
 		Where("name = ?", name).LoadStruct(tag); err != nil {
 		return nil, err
 	}
 	return tag, nil
 }
 
-func Tags(sess *dbr.Session) ([]*model.Tag, error) {
+func Tags(sr dbr.SessionRunner) ([]*model.Tag, error) {
 	tagList := make([]*model.Tag, 0)
-	if _, err := sess.Select("*").From(model.TagTableName).
+	if _, err := sr.Select("*").From(model.TagTableName).
 		LoadStructs(&tagList); err != nil {
 		return nil, err
 	}
 	return tagList, nil
 }
 
-func TagsByCount(sess *dbr.Session) ([]*model.Tag, error) {
+func TagsByCount(sr dbr.SessionRunner) ([]*model.Tag, error) {
 	tagList := make([]*model.Tag, 0)
-	if _, err := sess.Select("Tag.id, Tag.name").
+	if _, err := sr.Select("Tag.id, Tag.name").
 		From(model.TagTableName).
 		Join(model.NoteTagTableName, "Tag.id = NoteTag.tag_id").
 		GroupBy("Tag.id").
@@ -46,9 +46,9 @@ func TagsByCount(sess *dbr.Session) ([]*model.Tag, error) {
 	return tagList, nil
 }
 
-func TagsByNoteId(sess *dbr.Session, noteId int64) ([]*model.Tag, error) {
+func TagsByNoteId(sr dbr.SessionRunner, noteId int64) ([]*model.Tag, error) {
 	tagList := make([]*model.Tag, 0)
-	if _, err := sess.Select("Tag.id", "Tag.name").From(model.TagTableName).
+	if _, err := sr.Select("Tag.id", "Tag.name").From(model.TagTableName).
 		Join(model.NoteTagTableName, "Tag.id = NoteTag.tag_id").Where("note_id = ?", noteId).
 		LoadStructs(&tagList); err != nil {
 		return nil, err
@@ -56,10 +56,10 @@ func TagsByNoteId(sess *dbr.Session, noteId int64) ([]*model.Tag, error) {
 	return tagList, nil
 }
 
-func TagsByNoteIds(sess *dbr.Session, noteIds []int64) (map[int64][]*model.Tag, error) {
+func TagsByNoteIds(sr dbr.SessionRunner, noteIds []int64) (map[int64][]*model.Tag, error) {
 	noteIdTagList := make([]*model.NoteIdTag, 0)
 
-	if _, err := sess.Select("NoteTag.note_id note_id", "Tag.id tag_id", "Tag.Name tag_name").
+	if _, err := sr.Select("NoteTag.note_id note_id", "Tag.id tag_id", "Tag.Name tag_name").
 		From(model.TagTableName).Join(model.NoteTagTableName, "Tag.id = NoteTag.tag_id").
 		Where("note_id in ?", noteIds).LoadStructs(&noteIdTagList); err != nil {
 		return nil, err
@@ -81,9 +81,9 @@ func TagsByNoteIds(sess *dbr.Session, noteIds []int64) (map[int64][]*model.Tag, 
 	return tagListMap, nil
 }
 
-func SelectTagOrInsertIfNotExists(tx *dbr.Tx, tag *model.Tag) (*model.Tag, error) {
+func SelectTagOrInsertIfNotExists(sr dbr.SessionRunner, tag *model.Tag) (*model.Tag, error) {
 	found := true
-	if err := tx.Select("*").From(model.TagTableName).
+	if err := sr.Select("*").From(model.TagTableName).
 		Where("name = ?", tag.Name).LoadStruct(tag); err != nil {
 		if err == dbr.ErrNotFound {
 			found = false
@@ -92,7 +92,7 @@ func SelectTagOrInsertIfNotExists(tx *dbr.Tx, tag *model.Tag) (*model.Tag, error
 		}
 	}
 	if !found {
-		result, err := tx.InsertInto(model.TagTableName).Columns("name").Record(tag).Exec()
+		result, err := sr.InsertInto(model.TagTableName).Columns("name").Record(tag).Exec()
 		if err != nil {
 			return nil, err
 		}
