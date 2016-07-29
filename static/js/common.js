09468522ff54f81f4dce56ce6b6d9ffb121533b2
Zhang Weihu <496781108@qq.com>
2016-07-29 17:46:08 +0800

    fake loading

--- static/js/common.js
+++ static/js/common.js
@@ -4,6 +4,15 @@
 			NProgress.start();
 			$('.search-icon').hide();
 			$('.search-loading').show();
-		})
+		});
+		$(window).on('beforeunload', function(){
+			NProgress.configure({
+				showSpinner: true,
+				trickle: true,
+				minimum: 0.08
+			});
+			NProgress.start();
+			NProgress.set(0);
+		});
 	});
 })(jQuery);
