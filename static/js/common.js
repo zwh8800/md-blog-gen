e75f83d91451bcc0c0a2b0bd9fe1491e8daaa34f
Zhang Weihu <496781108@qq.com>
2017-08-19 09:44:48 +0800

    scroll

--- static/js/common.js
+++ static/js/common.js
@@ -1,31 +1,31 @@
 (function ($) {
-	$(function () {
-		$('.search-form').submit(function () {
-			NProgress.start();
-			$('.search-icon').hide();
-			$('.search-loading').show();
-		});
-		$(window).on('beforeunload', function(){
-			NProgress.configure({
-				showSpinner: true,
-				trickle: true,
-				minimum: 0.08
-			});
-			NProgress.start();
-			NProgress.set(0);
-		});
-		$("#keyword").autocomplete({
-			source: function(request, response) {
-				$.get("/api/search/" + encodeURIComponent(request.term), function(data) {
-					data.push({
-						id: 0,
-						value: '查看更多...'
-					});
-					response(data);
-				});
-			},
-			select: function(event, ui) {
-				event.preventDefault();
+    $(function () {
+        $('.search-form').submit(function () {
+            NProgress.start();
+            $('.search-icon').hide();
+            $('.search-loading').show();
+        });
+        $(window).on('beforeunload', function () {
+            NProgress.configure({
+                showSpinner: true,
+                trickle: true,
+                minimum: 0.08
+            });
+            NProgress.start();
+            NProgress.set(0);
+        });
+        $("#keyword").autocomplete({
+            source: function (request, response) {
+                $.get("/api/search/" + encodeURIComponent(request.term), function (data) {
+                    data.push({
+                        id: 0,
+                        value: '查看更多...'
+                    });
+                    response(data);
+                });
+            },
+            select: function (event, ui) {
+                event.preventDefault();
                 var $keyword = $("#keyword");
                 if (ui.item.id === 0) {
                     window.location.href = "/search/" + encodeURIComponent($keyword.val());
@@ -33,26 +33,33 @@
                 }
                 $keyword.val(ui.item.value.replace(/<em>/g, '').replace(/<\/em>/g, ''));
                 window.location.href = "/note/" + ui.item.id;
-			},
-            focus: function(event, ui) {
+            },
+            focus: function (event, ui) {
                 event.preventDefault();
             },
-            open: function(event, ui) {
-				if ($(window).width() > 700) {
+            open: function (event, ui) {
+                if ($(window).width() > 700) {
                     $(this).autocomplete("widget").css({
                         "width": 195
                     });
                 }
             }
 
-		}).data('ui-autocomplete')._renderItem = function( ul, item ) {
+        }).data('ui-autocomplete')._renderItem = function (ul, item) {
             return $('<li class="ui-menu-item"></li>')
                 .data("ui-autocomplete-item", item)
-				.append(
-					$('<div class="ui-menu-item-wrapper"></div>').
-					append(item.label)
-				)
+                .append(
+                    $('<div class="ui-menu-item-wrapper"></div>').append(item.label)
+                )
                 .appendTo(ul);
         };
-	});
+        if ($(window).width() <= 700) {
+            $("html, body").animate({
+                scrollTop: $('.search-box').outerHeight() + "px"
+            }, {
+                duration: 500,
+                easing: "swing"
+            });
+        }
+    });
 })(jQuery);
